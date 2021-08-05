c0a6f8289ac24b2e50ba381abd6fe1f37c61d52b
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-05 20:20:29 -0300

    Conversor dolar

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,1 +1,4 @@
 #conversor de moedas
+n = float(input('Digite um valor em Reais R$:'))
+print('Considerando a cotação atual (05/08/2021) USD$: 1 = R$: 5.25 ...')
+print('Com {:.2f} Reais, você tem {:.2f} Dorales!'.format(n, n/5.25))
