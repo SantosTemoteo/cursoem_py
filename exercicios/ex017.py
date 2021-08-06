8ee5d348de4094406d9580edd374fa265e41bb13
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-06 20:10:03 -0300

    Hipotenusa

--- exercicios/ex017.py
+++ exercicios/ex017.py
@@ -1,5 +1,11 @@
+'''
 from math import sqrt
 catop = float(input('Cateto Oposto: '))
 catadj = float(input('Cateto Adjacente: '))
 hip = (catop**2)+(catadj**2) # **(1/2)
 print('Hipotenusa = {:.2f}'.format(sqrt(hip)))
+'''
+import math
+catop = float(input('Cateto Oposto: '))
+catadj = float(input('Cateto Adjacente: '))
+print('Hipotenusa = {:.2f}'.format(math.hypot(catop, catadj)))
