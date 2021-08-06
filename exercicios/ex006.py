9af8a75dbd45a5170f815000bcdc882e8c44d3ea
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-05 23:56:00 -0300

    Raiz Quadrada

--- exercicios/ex006.py
+++ exercicios/ex006.py
@@ -1,6 +1,6 @@
-import math
+from math import sqrt
 n = float(input('Digite um número: '))
 print('Dobro = {}'.format(n*2))
 print('Tripulo = {}'.format(n*3))
-print('Raiz quadrada = {}'.format(math.sqrt(n)))
+print('Raiz quadrada = {}'.format(sqrt(n)))
 # raiz =  n ** (1/2)
