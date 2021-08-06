bc1ffff2dd0062c0a6b296900d2ba1b7bbb01e5c
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-05 21:01:38 -0300

    Economizando Tinta

--- exercicios/ex011.py
+++ exercicios/ex011.py
@@ -3,4 +3,4 @@ altura = float(input('Digite ALTURA da parede em metros: '))
 largura = float(input('Digite a LARGURA da parede em metros: '))
 area = altura*largura
 print('Área a ser pintada = {} m²'.format(area))
-print('{} litros necessários para pintar a parede.'.format(area*2))
+print('{} litros necessários para pintar a parede.'.format(area/2))
