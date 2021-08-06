bddd524755bb3f0aafa8dbdd60f940dc56f906e6
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-06 02:53:49 -0300

    Scin Cos Tan

---
+++ exercicios/ex018.py
@@ -0,0 +1,5 @@
+import math
+n = float(input('Digite um ângulo qualquer: '))
+print('Seno de {} = {}'.format(n, math.sin(n)))
+print('Cosseno de {} = {}'.format(n, math.cos(n)))
+print('Tangente de {} = {}'.format(n, math.tan(n)))
