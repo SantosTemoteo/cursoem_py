1cd3231e39066d5c0cd376778313f92a35f8dffe
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-07 16:30:19 -0300

    Analizador de textos

--- exercicios/ex022.py
+++ exercicios/ex022.py
@@ -1,1 +1,7 @@
 #analizador de textos
+nome = str(input('Nome completo: ')).strip()
+print('Maiúsculo: {}'.format(nome.upper()))
+print('minúsculo: {}'.format(nome.lower()))
+div = nome.split()
+print('Quantidade de carácteres: {}'.format(len(''.join(div))))
+print('Quant. carac. da 1ª letra: {}'.format(len(div[0])))
