9544da6f7ef63a5926873db137f3603e073c74c8
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-07 19:40:17 -0300

    Separando Dígitos

---
+++ exercicios/ex023.py
@@ -0,0 +1,11 @@
+
+n = int(input('Informe um número: '))
+print('Analizando o número {}'.format(n))
+u = n // 1 % 10
+d = n // 10 % 10 # não entendo kkkkj
+c = n // 100 % 10
+m = n // 1000 % 10
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
