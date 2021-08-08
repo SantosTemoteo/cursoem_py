0b880d4178cb80236c611303cd5a9ce8c2fec309
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-08-07 23:49:18 -0300

    Verificando primeiras letras de um texto

--- exercicios/ex024.py
+++ exercicios/ex024.py
@@ -1,2 +1,3 @@
-n = str(input('Em que estado voce mora? ')).strip()
-print(n[:5].upper() == 'SANTO') #gambi
+#gambi
+n = str(input('Em que estado voce mora? ')).lower().replace('-','').split()
+print(n[0] == 'santo')
